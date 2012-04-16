a096d4f0c6af29995f00ccd4e9bc351a27733e9c
Eliseo Ocampos <roskoff@gmail.com>
2012-04-15 22:51:20 -0400

    Scrapping de los datos usando jsdom. Actualización de los datos via setInterval

--- scrap_data.js
+++ scrap_data.js
@@ -1,41 +1,20 @@
 var http = require('http');
-
-function getNextMatchData(){
-
-    var data = 5;
-    var request = require('request');
-    request('http://www.franjeado.com/stats.php', function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-        //console.log(body) // Print the google web page.
-            data = body;
-            res.write(data);
-        }
-    })
-    var json_data = {}; //$(data).text();
-                
-     /*         
-    json_data["partido"] = doc.html.body.div.table.tr[1].td.table.tr.td.table[0].tr.td[1].p.getText().trim();
-    json_data["equipo_uno"] = doc.html.body.div.table.tr[1].td.table.tr.td.table[0].tr.td[1].div.table.tr[0].td.div.table.tr.td[1].div.getText().trim();
-    json_data["equipo_dos"] = doc.html.body.div.table.tr[1].td.table.tr.td.table[0].tr.td[1].div.table.tr[0].td.div.table.tr.td[3].div.getText().trim();  
-    json_data["estadio"] = doc.html.body.div.table.tr[1].td.table.tr.td.table[0].tr.td[1].div.table.tr[2].td[0].div.getText().trim();
-    json_data["fecha"] = doc.html.body.div.table.tr[1].td.table.tr.td.table[0].tr.td[1].div.table.tr[2].td[1].div.getText().trim();
-    json_data["hora"] = doc.html.body.div.table.tr[1].td.table.tr.td.table[0].tr.td[1].div.table.tr[2].td[2].div.getText().trim();
-    json_data["entradas"] = doc.html.body.div.table.tr[1].td.table.tr.td.table[0].tr.td[1].div.table.tr[4].td[0].div.getText().trim();
-    json_data["arbitro"] = doc.html.body.div.table.tr[1].td.table.tr.td.table[0].tr.td[1].div.table.tr[4].td[1].div.getText().trim();
-    */
-
-    //return json_data;
-    return data;
-
-}
-
 var request = require('request');
-var $$ = require('jquery');
 var jsdom = require('jsdom');
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+var MY_APP = {};
+MY_APP["json_data"] =  {"partido": "--",
+                        "equipo_uno": {"nombre": "--", 
+                                       "icono": ""},
+                        "equipo_dos": {"nombre": "--", 
+                                       "icono": ""},
+                        "estadio": "--",
+                        "fecha": "--",
+                        "hora": "--",
+                        "entradas": "--",
+                        "arbitro": "--"};
 
+function getNextMatchData(){
     request('http://www.franjeado.com/stats.php', function (e, r, body) {
         if (!e && r.statusCode == 200) {
             jsdom.env({
@@ -45,7 +24,7 @@ http.createServer(function (req, res) {
                 ],
                 done: function(errors, window) {
                     var $ = window.$;
-                    var json_data = {};
+                    var json_data = MY_APP["json_data"];
                     // Eliminamos la viñeta que tiene antes del texto
                     $('body > div > table > tr:eq(1) > td > table > tr > td > table:first > tr > td:eq(1) > p > strong').remove();
                     // Eliminamos el link del enlace a la extensión OlimpiaNextMatch para Google Chrome
@@ -76,25 +55,23 @@ http.createServer(function (req, res) {
                     json_data["arbitro"] = $('body > div > table > tr:eq(1) > td > table > tr > td > table:first > tr > td:eq(1) > div > table > tr:eq(4) > td:eq(1) > div ' ).text().trim();
 
                     // Retornamos en el response los datos en formato JSON
-                    res.end(JSON.stringify(json_data));
+                    //res.end(JSON.stringify(json_data));
+                    console.log("json_data updated");
                 }
             });
-
-        } else {
-            var json_data = {"partido": "--",
-                             "equipo_uno": {"nombre": "--", 
-                                            "icono": ""},
-                             "equipo_dos": {"nombre": "--", 
-                                            "icono": ""},
-                             "estadio": "--",
-                             "fecha": "--",
-                             "hora": "--",
-                             "entradas": "--",
-                             "arbitro": "--"};
-            res.end(JSON.stringify(json_data));
-        }
+        } else { console.log("json_data NOT updated");  }
 
     });
+
+}
+
+// Actualizar datos cada 5 minutos
+setInterval(getNextMatchData, 300000);
+
+http.createServer(function (req, res) {
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(JSON.stringify(MY_APP["json_data"]));
 
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
